5b65218ed3905b1c7115ce327f6604b25c9521b5
Piyal Shah <piyalsha007@gmail.com>
2025-05-22 09:34:34 +0600

    added React Awesome reveal at home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,14 +3,23 @@ import Banner from '../Components/Banner';
 import RecentTasks from '../Components/RecentTasks';
 import Category from '../Components/Category';
 import TopFreelancer from '../Components/TopFreelancer';
+import { Fade, Slide } from 'react-awesome-reveal';
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Banner />
-      <RecentTasks />
-      <Category />
-      <TopFreelancer/>
+      <Fade>
+        <Banner />
+      </Fade>
+      <Slide direction="right">
+        <RecentTasks />
+      </Slide>
+      <Fade delay={200}>
+        <Category />
+      </Fade>
+      <Slide direction="left" delay={200}>
+        <TopFreelancer />
+      </Slide>
     </div>
   );
 };
