5224733aee30eab5bc755a2618b67543fa6827a6
Piyal Shah <piyalsha007@gmail.com>
2025-06-25 22:12:00 +0600

    make banner section full width

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Banner = () => {
     return (
         <>
-            <div className="w-11/12 mx-auto relative min-h-[400px] lg:min-h-[700px] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("https://i.postimg.cc/RVSTZyv8/banner.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div className="w-full relative min-h-[400px] lg:min-h-[700px] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("https://i.postimg.cc/RVSTZyv8/banner.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                 <div className="absolute inset-0 bg-black/60 sm:bg-black/50"></div>
-                <div className="relative w-11/12 mx-auto flex flex-col items-center justify-center min-h-[400px] lg:min-h-[600px] text-white text-center px-3 sm:px-6 lg:px-8">
+                <div className="relative w-full mx-auto flex flex-col items-center justify-center min-h-[400px] lg:min-h-[600px] text-white text-center px-3 sm:px-6 lg:px-8 max-w-7xl">
                     <h1 className="text-2xl text-3xl md:text-4xl lg:text-5xl font-bold mb-3 sm:mb-4">Hire a Freelancer for any task</h1>
                     <p className="text-base text-xs md:text-xl mb-6 sm:mb-8">Over 1200+ expert freelancers are waiting for you</p>
 
